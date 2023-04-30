bc4805d6a03af616f86aa3b649c1ac341899ee05
michaelfitts <90995090+michaelfitts@users.noreply.github.com>
2023-04-29 18:07:21 -0600

    really rough pipes

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Animated, TouchableWithoutFeedback, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Animated,
+  TouchableWithoutFeedback,
+  Text,
+  Dimensions,
+} from 'react-native';
 
 const BIRD_SIZE = 25;
 const GRAVITY = 9.8; // Acceleration due to gravity
 
+const PIPE_WIDTH = 60;
+const PIPE_HEIGHT = 120;
+
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT = Dimensions.get('window').width;
+
 function App() {
+  const getRandomGapY = () => {
+    const minGapY = PIPE_HEIGHT * 2;
+    const maxGapY = HEIGHT - PIPE_HEIGHT * 3;
+    return minGapY + Math.random() * (maxGapY - minGapY);
+  };
+
+  const Pipe = ({ pipeX, gapY }) => {
+    return (
+      <>
+        <Animated.View
+          style={[
+            styles.pipe,
+            { height: gapY - PIPE_HEIGHT / 2, left: pipeX },
+            { transform: [{ rotate: '180deg' }] },
+          ]}
+        />
+        <Animated.View
+          style={[
+            styles.pipe,
+            { height: HEIGHT - gapY - PIPE_HEIGHT / 2, left: pipeX, bottom: 0 },
+          ]}
+        />
+      </>
+    );
+  };
+
   const [birdY, setBirdY] = useState(new Animated.Value(0)); // Initial position of the ball
   const [currentPosition, setCurrentPosition] = useState(0); // Current position of the ball
+
+  const [pipes, setPipes] = useState([
+    { x: new Animated.Value(WIDTH), gapY: getRandomGapY() },
+    { x: new Animated.Value(WIDTH + WIDTH / 2), gapY: getRandomGapY() },
+    { x: new Animated.Value(WIDTH + WIDTH), gapY: getRandomGapY() },
+  ]);
+
+  const PIPE_SPEED = 5;
 
   useEffect(() => {
     Animated.timing(birdY, {
@@ -16,10 +63,28 @@ function App() {
     }).start(); // Start the animation
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPipes((pipes) => {
+        const newPipes = [...pipes];
+        for (let i = 0; i < newPipes.length; i++) {
+          newPipes[i].x.setValue(newPipes[i].x._value - PIPE_SPEED);
+          if (newPipes[i].x._value < -PIPE_WIDTH) {
+            newPipes[i].x.setValue(WIDTH);
+            newPipes[i].gapY = getRandomGapY();
+          }
+        }
+        return newPipes;
+      });
+    }, 1000 / 60);
+
+    return () => clearInterval(interval);
+  }, []);
+
   const handlePress = () => {
     //Jumping up
     Animated.timing(birdY, {
-      toValue: birdY._value - .3,
+      toValue: birdY._value - 0.3,
       duration: 500,
       useNativeDriver: true,
     }).start(() => {
@@ -50,8 +115,10 @@ function App() {
   return (
     <TouchableWithoutFeedback onPress={handlePress}>
       <View style={styles.container}>
+        {pipes.map(({ x, gapY }, i) => (
+          <Pipe key={i} pipeX={x} gapY={gapY} />
+        ))}
         <Animated.View style={[styles.bird, birdStyle]} />
-        
       </View>
     </TouchableWithoutFeedback>
   );
@@ -70,7 +137,12 @@ const styles = StyleSheet.create({
     width: BIRD_SIZE,
     borderRadius: BIRD_SIZE / 2,
   },
-  
+  pipe: {
+    position: 'absolute',
+    width: PIPE_WIDTH,
+    backgroundColor: 'green',
+  },
 });
 
 export default App;
+
